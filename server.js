51978bec87c08ca15a7a6f31c5a02f2365f26f63
Opilane TTHK <opilane@tthk.ee>
2020-10-20 10:31:47 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const errorPage = require('./routes/404');
+const adminRoute=require('./routes/admin');
+const app = express();
+
+
+app.set('view engine', ejs);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.use(adminRoute);
+app.use(errorPage);
+
+
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000.")
+});
